412fbb33e93c5a5dd864f8747fa7001d8e4b85e2
wwng <17147265+wwng2333@users.noreply.github.com>
2024-03-24 21:47:05 +0800

    add EXTI0 as event

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -125,14 +125,24 @@ void MX_GPIO_Init(void)
 
   /**/
   LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTC, LL_EXTI_CONFIG_LINE14);
-
+	
+  /**/
+  LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE0);
+	
   /**/
   EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_14;
   EXTI_InitStruct.LineCommand = ENABLE;
   EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
   EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING_FALLING;
   LL_EXTI_Init(&EXTI_InitStruct);
-
+	
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_EVENT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+	
   /**/
   LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_14, LL_GPIO_PULL_NO);
 
